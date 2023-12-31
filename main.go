4a238fefa6165f733118439e3f50b6e0b374ae70
Alexander Perez <alexander.pereze02@gmail.com>
2023-12-31 10:44:31 -0500

    chore: add updateMovie method

--- main.go
+++ main.go
@@ -56,13 +56,27 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 
 func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	var movie Movie 
+	var movie Movie
 	_ = json.NewDecoder(r.Body).Decode(&movie)
-	fmt.Println(movie)
-	fmt.Println(r.Body)
 	movie.ID = strconv.Itoa(rand.Intn(1000000))
 	movies = append(movies, movie)
 	json.NewEncoder(w).Encode(movie)
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var movie Movie
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
 }
 
 func main() {
