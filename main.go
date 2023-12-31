f3d627c47e9837f1a590c1ccef6f9e469ec2150c
Alexander Perez <alexander.pereze02@gmail.com>
2023-12-30 22:56:26 -0500

    chore: add getMovies & deleteMovies methods

--- main.go
+++ main.go
@@ -24,9 +24,26 @@ type Director struct{
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, item := range movies {
+		if item.ID == params["id"] {
+			// (↓) to keep the slice in ORDER, we shift all of the elements at the right of the deleting index by one to the left
+			movies = append(movies[:index], movies[index+1:]...) 
+			break
+		}
+	}
+}
+
 func main() {
 	r := mux.NewRouter()
-
+	
 	movies = append(movies, Movie{ID: "1", Isbn: "123456", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "123457", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
 	
@@ -35,7 +52,8 @@ func main() {
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
-
+	
 	fmt.Println("Starting server on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
+
