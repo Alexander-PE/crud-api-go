64f56afb9e40e014f7b5e454e80e5aec26dd8968
Alexander Perez <alexander.pereze02@gmail.com>
2023-12-31 09:40:51 -0500

    chore: add createMovie method

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Movie struct {
@@ -49,6 +52,17 @@ func getMovie(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var movie Movie 
+	_ = json.NewDecoder(r.Body).Decode(&movie)
+	fmt.Println(movie)
+	fmt.Println(r.Body)
+	movie.ID = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(w).Encode(movie)
 }
 
 func main() {
