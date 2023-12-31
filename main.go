76a8a8bc5af9a24771f891531667c8731aaea43e
Alexander Perez <alexander.pereze02@gmail.com>
2023-12-31 09:34:34 -0500

    chore: Add getMovie method

--- main.go
+++ main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"encoding/json"
-	"math/rand"
-	"net/http"
-	"strconv"
+	"fmt"
 	"github.com/gorilla/mux"
+	"log"
+	"net/http"
 )
 
 type Movie struct {
-	ID string `json:"id"`
-	Isbn string `json:"isbn"`
-	Title string `json:"title"`
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
 	Director *Director `json:"director"`
 }
 
-type Director struct{
+type Director struct {
 	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
+	Lastname  string `json:"lastname"`
 }
 
 var movies []Movie
@@ -35,25 +33,36 @@ func deleteMovie(w http.ResponseWriter, r *http.Request) {
 	for index, item := range movies {
 		if item.ID == params["id"] {
 			// (↓) to keep the slice in ORDER, we shift all of the elements at the right of the deleting index by one to the left
-			movies = append(movies[:index], movies[index+1:]...) 
+			movies = append(movies[:index], movies[index+1:]...)
 			break
+		}
+	}
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range movies {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
 		}
 	}
 }
 
 func main() {
 	r := mux.NewRouter()
-	
+
 	movies = append(movies, Movie{ID: "1", Isbn: "123456", Title: "Movie One", Director: &Director{Firstname: "John", Lastname: "Doe"}})
 	movies = append(movies, Movie{ID: "2", Isbn: "123457", Title: "Movie Two", Director: &Director{Firstname: "Steve", Lastname: "Smith"}})
-	
+
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
 	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
-	
+
 	fmt.Println("Starting server on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
-
